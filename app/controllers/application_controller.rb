1c2ec28ec32502d8649ba262fbf9bb6711a708c5
Scott Conley <scranky@orangejello.local>
2010-10-27 14:14:45 -0400

    hiding log msgs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
+  EXCEPTIONS_NOT_LOGGED = ['ActionController::RoutingError']
+
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+  protected
+    def log_error(exc)
+      super unless EXCEPTIONS_NOT_LOGGED.include?(exc.class.name)
+    end
+    
 end
