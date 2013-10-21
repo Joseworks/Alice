ebd80009ff3813cb9bfa4b303664876365229860
Patrick Kennedy <bigredtriangle@gmail.com>
2013-10-21 14:23:24 -0400

    initial adding of disqus comments

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -10,4 +10,29 @@ module PostsHelper
       "Updated:#{post.edited_at.strftime("%l:%M, %b %d")}"
     end
   end
+
+  def disqus_comments
+    javascript_tag("/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'quidnunc'; // required: replace example with your forum shortname
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();")
+  end
+
+  def disqus_comment_counts
+    javascript_tag("/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+      var disqus_shortname = 'quidnunc'; // required: replace example with your forum shortname
+
+      /* * * DON'T EDIT BELOW THIS LINE * * */
+      (function () {
+      var s = document.createElement('script'); s.async = true;
+      s.type = 'text/javascript';
+      s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+      (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+      }());")
+  end
 end
