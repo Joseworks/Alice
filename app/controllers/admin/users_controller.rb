22ba1e493cd15495d8a08bd085f697dde044016b
Patrick Kennedy <bigredtriangle@gmail.com>
2013-10-22 16:42:19 -0400

    add role to users and user admin panel

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,34 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => :update
+
 
   def index
     @users = User.all
   end
+
+  def update
+    if @user.update_attributes(user_params)
+      respond_to do |format|
+        format.html {
+          flash[:notice] = "Updated user '#{@user.name}'"
+          redirect_to(:action => 'index')
+        }
+      end
+    else
+      respond_to do |format|
+        format.html { render :action => 'index', :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+      def user_params
+      params.require(:user).permit(:name, :email, :role)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022165550) do
+ActiveRecord::Schema.define(version: 20131022190220) do
 
   create_table "comments", force: true do |t|
     t.integer  "post_id",      null: false
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 20131022165550) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "role"
   end
 
 end
