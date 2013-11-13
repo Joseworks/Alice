b867756724042c000994edda3cc74b326a9628a5
Patrick Kennedy <bigredtriangle@gmail.com>
2013-11-13 10:54:31 -0500

    remove Cucumber since it we weren't really using it much anyways

--- features/step_definitions/posts.rb
+++
@@ -1,7 +0,0 @@
-Given /there is at least one post tagged "([\w\s]+)"/ do |tag_name|
-  FactoryGirl.create(:post, :tag_list => 'awesome')
-end
-
-Given /there is at least one post titled "([\w\s]+)"/ do |title|
-  FactoryGirl.create(:post, :title => title)
-end
