9e7166bc664b0e4716316a1fcd9c13e196df50a5
Patrick Kennedy <bigredtriangle@gmail.com>
2014-01-13 09:49:40 -0500

    make @top_posts in post_controller limit to start of day before

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,7 @@ class PostsController < ApplicationController
     @tag = params[:tag]
     @tags = Post.tag_counts_on(:tags).order('count desc').limit(15)
     @posts = Post.find_recent(tag: @tag, include: :tags, page: params[:page])
-    # @top_posts = Post.where("created_at >= ?", (Time.zone.now.beginning_of_day - 3)).order('posts.impressions_count DESC').limit(3)
-    @top_posts = Post.order('posts.impressions_count DESC').limit(3)
+    @top_posts = Post.where("created_at >= ?", (Time.zone.now.beginning_of_day - 1)).order('posts.impressions_count DESC').limit(3)
 
     respond_to do |format|
       format.html
@@ -16,8 +15,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {include: [:tags]}))
     @tags = Post.tag_counts_on(:tags).order('count desc').limit(15)
-    # @top_posts = Post.where("created_at >= ?", (Time.zone.now.beginning_of_day - 3)).order('posts.impressions_count DESC').limit(3)
-    @top_posts = Post.order('posts.impressions_count DESC').limit(3)
+    @top_posts = Post.where("created_at >= ?", (Time.zone.now.beginning_of_day - 1)).order('posts.impressions_count DESC').limit(3)
 
 
     impressionist @post
