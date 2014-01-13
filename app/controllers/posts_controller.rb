437512f480223b871bf88429a49bbe8a32b0522c
Patrick Kennedy <bigredtriangle@gmail.com>
2014-01-13 12:45:01 -0500

    refactor tag_cloud controller method for PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,9 @@
 class PostsController < ApplicationController
+  before_filter :tag_cloud
 
   def index
     @tag = params[:tag]
-    @tags = Post.tag_counts_on(:tags).order('count desc').limit(15)
+    # @tags = Post.tag_counts_on(:tags).order('count desc').limit(15)
     @posts = Post.find_recent(tag: @tag, include: :tags, page: params[:page])
     @top_posts = Post.where("created_at >= ?", (Time.zone.now.beginning_of_day - 1)).order('posts.impressions_count DESC').limit(3)
 
@@ -14,14 +15,15 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {include: [:tags]}))
-    @tags = Post.tag_counts_on(:tags).order('count desc').limit(15)
+    # @tags = Post.tag_counts_on(:tags).order('count desc').limit(15)
     @top_posts = Post.where("created_at >= ?", (Time.zone.now.beginning_of_day - 1)).order('posts.impressions_count DESC').limit(3)
 
 
     impressionist @post
   end
 
-  def tag_cloud
-    @tags = Post.tag_counts_on(:tags)
-  end
+  private
+    def tag_cloud
+      @tags = Post.tag_counts_on(:tags).order('count desc').limit(15)
+    end
 end
