3391612dc5736669d112faa2a70bccca2bb7ddad
Patrick Kennedy <bigredtriangle@gmail.com>
2014-01-15 10:48:12 -0500

    refactor top_posts for post_controller to before_filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   before_filter :tag_cloud
+  before_filter :top_posts
 
   def index
     @tag = params[:tag]
     @posts = Post.find_recent(tag: @tag, include: :tags, page: params[:page])
-    @top_posts = Post.where("published_at >= ?", (Time.zone.now.beginning_of_day - 4)).order('posts.impressions_count DESC').limit(3)
 
     respond_to do |format|
       format.html
@@ -14,8 +14,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {include: [:tags]}))
-    @top_posts = Post.where("published_at >= ?", (Time.zone.now.beginning_of_day - 4)).order('posts.impressions_count DESC').limit(3)
-
 
     impressionist @post
   end
@@ -23,5 +21,9 @@ class PostsController < ApplicationController
   private
     def tag_cloud
       @tags = Post.tag_counts_on(:tags).order('count desc').limit(15)
+    end
+
+    def top_posts
+      @top_posts = Post.where("published_at >= ?", (Time.zone.now.beginning_of_day - 3.days)).order('posts.impressions_count DESC').limit(3)
     end
 end
