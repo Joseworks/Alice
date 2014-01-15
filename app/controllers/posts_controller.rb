e100e0c938e974cf19a7c3e723c8f419f7e344fa
Patrick Kennedy <bigredtriangle@gmail.com>
2014-01-15 10:39:20 -0500

    improve scope of @top_posts in postcontroller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   def index
     @tag = params[:tag]
     @posts = Post.find_recent(tag: @tag, include: :tags, page: params[:page])
-    @top_posts = Post.where("created_at >= ?", (Time.zone.now.beginning_of_day - 1)).order('posts.impressions_count DESC').limit(3)
+    @top_posts = Post.where("published_at >= ?", (Time.zone.now.beginning_of_day - 4)).order('posts.impressions_count DESC').limit(3)
 
     respond_to do |format|
       format.html
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by_permalink(*([:year, :month, :day, :slug].collect {|x| params[x] } << {include: [:tags]}))
-    @top_posts = Post.where("created_at >= ?", (Time.zone.now.beginning_of_day - 1)).order('posts.impressions_count DESC').limit(3)
+    @top_posts = Post.where("published_at >= ?", (Time.zone.now.beginning_of_day - 4)).order('posts.impressions_count DESC').limit(3)
 
 
     impressionist @post
