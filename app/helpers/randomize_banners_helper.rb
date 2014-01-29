f5e4ebe241d362422b7cb32458d7c700cdfb648c
Patrick Kennedy <bigredtriangle@gmail.com>
2014-01-29 15:07:41 -0500

    adjustments to randomizeBannersHelper and test

--- app/helpers/randomize_banners_helper.rb
+++ app/helpers/randomize_banners_helper.rb
@@ -1,6 +1,6 @@
 module RandomizeBannersHelper
   def randomize_side_banner
     all_banners = ['Q-WebAd-2-250.png', 'Q-WebAd-250.png', 'Q-WebAd-4-250.png', 'Q-WebAd-5-250.png']
-    random_banner = all_banners.sample.to_s
+    random_banner = all_banners.sample
   end
 end
