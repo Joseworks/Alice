200143bd13c9efb10759654d5d53f4420f8d08a0
Jose C Fernandez <donjciii@gmail.com>
2014-02-11 12:25:19 -0500

    Moving all the devise sessions views  from admin sessions to users since it is not in use by the controller anymore
    Removed helper   # helper_method :current_user from application_controller.rb for devise to use his own current_user.
    
    Modified routes to devise_for :users, :path =>'',  :controllers => { :omniauth_callbacks => "omniauth_callbacks” }
    
    Created OmniauthCallbacksController
    
     Modified model for omniauth authentication through devise.
    
    Eliminated Omniauth.rb initializer

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '859497051931.apps.googleusercontent.com', 'iU6-Lt_tm5GBYBdmAPtAilHo', hd: 'quidnuncre.com'
-end
