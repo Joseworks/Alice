8635f9b486c188efd8c1eacc3fbcaf34ff8f3668
Patrick Kennedy <bigredtriangle@gmail.com>
2014-02-24 11:45:45 -0500

    adjust omniauth_callbacks_controller to require quidnuncre.com email addresses to login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,7 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-def google_oauth2
-
+  def google_oauth2
     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
-    if @user.persisted?
+    if @user.email.split("@")[1] == "quidnuncre.com" && @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
       sign_in_and_redirect @user, :event => :authentication
     else
@@ -10,4 +9,5 @@ def google_oauth2
       redirect_to new_user_session_url
     end
   end
+
 end
