6487297d4f4e7c04de907e38577539d555570220
Jose C Fernandez <donjciii@gmail.com>
2014-03-03 14:01:38 -0500

    Added the title of the feeds with formatting in front of the feeds line. Added extra feeds.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,11 +1,8 @@
 class FeedsController < ApplicationController
+before_filter :feeds, :only => :index
 
   # GET /feeds
   def index
-    @first_feed = SimpleRSS.parse open('http://slashdot.org/index.rdf')
-    @second_feed = SimpleRSS.parse open('http://slashdot.org/index.rdf')
-    @third_feed = SimpleRSS.parse open('http://gigaom.com/feed/')
-    @all_feeds = [@first_feed, @second_feed, @third_feed]
         respond_to do |format|
       format.html
       format.atom { render layout: false }
@@ -22,4 +19,21 @@ class FeedsController < ApplicationController
     def feed_params
       params[:feed]
     end
+
+  def feeds
+    @first_feed = SimpleRSS.parse open('http://feeds.feedburner.com/trdnews?format=xml')
+    @second_feed = SimpleRSS.parse open('http://ny.curbed.com/atom.xml')
+    @third_feed = SimpleRSS.parse open('http://www.dnainfo.com/new-york/index/all')
+    @fourth_feed =  SimpleRSS.parse open('http://feeds.crainsnewyork.com/crainsnewyork/deals')
+    @fifth_feed =  SimpleRSS.parse open('http://feeds.feedburner.com/inmannews?format=xml')
+    @sixth_feed =    SimpleRSS.parse open('http://www.ny1.com/Rss/Feeds.aspx?SecID=20090&RegionCookie=1')
+    @seventh_feed =  SimpleRSS.parse open('http://rss.nytimes.com/services/xml/rss/nyt/RealEstate.xml')
+    @eighth_feed =  SimpleRSS.parse open('http://rss.nytimes.com/services/xml/rss/nyt/Commercial.xml')
+    @ninth_feed =  SimpleRSS.parse open('http://www.costar.com/News/RSS/RSS.aspx?m=NYC')
+
+
+    @all_feeds = [@first_feed, @second_feed, @third_feed, @fourth_feed, @fifth_feed,
+                  @sixth_feed, @seventh_feed,@eighth_feed, @ninth_feed]
+  end
+
 end
--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -3,26 +3,27 @@ require 'spec_helper'
 describe FeedsController do
 
   describe "GET index" do
+    before (:each) do
+      @first_feed = {:title=>"Kepler's Alien World Count Skyrockets",
+              :link=>"http://rss.slashdot.org/~r/Slashdot/slashdot/to/~3/Opg2H9HUTIo/keplers-alien-world-count-skyrockets",
+              :description=>"&lt;img src=\"http://feeds.feedburner.com/~r/Slashdot/slashdot/to/~4/Opg2H9HUTIo\" height=\"1\" width=\"1\"/&gt;",
+              :feedburner_origLink=>"http://science.slashdot.org/story/14/02/26/2025231/keplers-alien-world-count-skyrockets?utm_source=rss0.9mainlinkanon&amp;utm_medium=feed"}
 
-    it "should be successful" do
+      SimpleRSS.stub(:parse).and_return(@first_feed)
       get :index
+    end
+    it "should be successful" do
+
       response.should be_success
     end
 
     it "should render index template" do
-      get :index
+
       response.should render_template('index')
     end
 
     it "should return a feed" do
-      first_feed = {:title=>"Kepler's Alien World Count Skyrockets",
-                    :link=>"http://rss.slashdot.org/~r/Slashdot/slashdot/to/~3/Opg2H9HUTIo/keplers-alien-world-count-skyrockets",
-                    :description=>"&lt;img src=\"http://feeds.feedburner.com/~r/Slashdot/slashdot/to/~4/Opg2H9HUTIo\" height=\"1\" width=\"1\"/&gt;",
-                    :feedburner_origLink=>"http://science.slashdot.org/story/14/02/26/2025231/keplers-alien-world-count-skyrockets?utm_source=rss0.9mainlinkanon&amp;utm_medium=feed"}
-
-      SimpleRSS.stub(:parse).and_return(first_feed)
-      get :index
-      assigns(:all_feeds).should include(first_feed)
+      assigns(:all_feeds).should include(@first_feed)
     end
   end
 end
