f9a887202a7a0de3fd742cdfa055e4337fd61927
Jose C Fernandez <donjciii@gmail.com>
2014-03-10 16:44:40 -0400

    Added some tests to the feed.rb.

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Feed do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    def valid_feed_attributes
+      { :uri => "http://www.costar.com/News/RSS/RSS.aspx?m=NYC",
+        :parsed_feed => { :uri => "http://www.costar.com/News/RSS/RSS.aspx?m=NYC",
+                          :title => "New York City Commercial Real Estate News From CoStar Group",
+                          :items => [{:title=>"CoStar's People of Note (Mar. 2 - 8)",
+                          :published => nil,
+                          :link => "http://www.costar.com/News/Article/CoStars-People-of-Note-Mar-2-8/158134?ref=/News/Article/CoStars-People-of-Note-Mar-2-8/158134&src=rss"}]
+                         }
+      }
+    end
+  end
+
+  describe Feed, 'validations' do
+    it 'serialize a parsed feed' do
+      feed= Feed.create(valid_feed_attributes)
+      serial_feed = feed.serializable_hash
+      expect(feed).to eql(serial_feed)
+    end
+
+    it 'is valid with valid_page_attributes' do
+      Feed.create(valid_feed_attributes).should be_valid
+    end
+
+    it 'is invalid with no uri' do
+      Feed.create(valid_feed_attributes.merge(:uri => '')).should_not be_valid
+    end
+
+    it 'is invalid with no parsed feed' do
+      Feed.create(valid_feed_attributes.merge(:parsed_feed => '')).should_not be_valid
+    end
+  end
 end
+
